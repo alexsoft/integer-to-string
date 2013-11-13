f9676b451d0e7d2f44982283ae6d94ed632e96f4
alexsoft <me@aplekhanov.org>
2013-11-13 02:06:25 +0200

    done

--- inttostr.js
+++ inttostr.js
@@ -1,5 +1,4 @@
 (function(){
-
 	var vocabulary = {
 		0: ['',       '',            '',          ''],
 		1: ['один',   '',            'сто',       'одна тысяча'],
@@ -11,9 +10,8 @@
 		7: ['семь',   'семьдесят',   'семсот',    'семь тысяч'],
 		8: ['восемь', 'восемьдесят', 'восемьсот', 'восемь тысяч'],
 		9: ['девять', 'девяносто',   'девятсот',  'девять тысяч']
-	};
-
-	var vocabulary_additional = {
+	},
+		vocabulary_additional = {
 		0: 'десять',
 		1: 'одиннадцать',
 		2: 'двенадцать',
@@ -25,7 +23,6 @@
 		8: 'восемнадцать',
 		9: 'девятнадцать'
 	};
-
 	var inttostr = function(t) {
 		var k = t.toString(),
 			str = '';
@@ -35,21 +32,19 @@
 			var digits = k.length;
 			for (var i = 1; i <= digits; i++) {
 				if (typeof(k[digits - i]) !== undefined) {
-
 					if (i === 2 && k[digits-i] === '1') {
 						str = vocabulary_additional[k[digits-1]];
 					}
-
 					str = vocabulary[k[digits-i]][i-1] + ' ' + str;
 					str = str.replace('  ', ' ');
 				} else {
 					break;
 				}
 			}
-
 			console.log(str);
 		}
 	}
-
-	inttostr(9907);
+	// Здесь можно вызывать ф-цию
+	// и результат будет выведен в консоль
+	inttostr(5231);
 })()
